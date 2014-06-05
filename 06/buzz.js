628a184742afea165f344378ae82c4ee5a2f8ece
hiroakiasada <t13023ha@zmac095.sfc.keio.ac.jp>
2014-06-05 11:40:33 +0900

    テスト終わりました

---
+++ 06/buzz.js
@@ -0,0 +1,9 @@
+var buzz = function(a){
+if(a%5==0){
+return ('buzz');
+}else if(a%5!=0){
+return a;
+}else{
+return a;
+}
+}
---
+++ 08/find_first_even_number.js
@@ -0,0 +1,9 @@
+function findFirstEvenNumber(a){
+a =array("1","2","3","4");
+var = i;
+for(i=0;i<4;i++){
+if(a[i]%2==0){
+return
+
+
+}
